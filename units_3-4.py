d0665e578ccd8bab8416cc5e1529f637bef588e5
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-01-09 10:40:53 +0300

    add unit 3

---
+++ units_3-4.py
@@ -0,0 +1,10 @@
+# -------- Предикат делимости --------
+# def func(num1, num2):
+#     return 'делится' if num1 % num2 == 0 else 'не делится'
+#
+#
+# print(func(10, 2))
+# print(func(5, 7))
+# print(func(15, 15))
+
+
