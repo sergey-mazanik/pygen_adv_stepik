51da019eb3bd739887571d00804856e128c5a51c
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-01-26 15:42:03 +0300

    add some tasks for unit 8

---
+++ unit_8.py
@@ -0,0 +1,17 @@
+""" Тимур и его команда """
+
+# n, m, k, x, y, z = [int(input()) for _ in range(6)]
+# count = n + m + k + z - x - y
+# print(count)
+
+""" Книги на прочтение """
+
+# n, m, k, x, y, z, t, a = [int(input()) for _ in range(8)]
+# s1 = -1 * (x - n - m) - t
+# s2 = -1 * (y - m - k) - t
+# s3 = -1 * (z - n - k) - t
+# two_books = s1 + s2 + s3
+# one_book = (n - s1 - s3 - t) + (m - s2 - s3 - t) + (k - s1 - s2 - t)
+# no_books = a - t - two_books - one_book
+#
+# print(one_book, two_books, no_books, sep='\n')
