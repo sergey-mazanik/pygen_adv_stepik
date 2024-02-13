e283f06f412f37825a49a82f54614b4ea3508e9a
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-02-13 11:22:33 +0300

    add some tasks for unit 10

--- unit_10.py
+++ unit_10.py
@@ -291,3 +291,39 @@
 #     n = dict_str[letter]
 #     new_str += my_dict[str(n)]
 # print(new_str)
+
+
+# numbers = [34, 10, -4, 6, 10, 23, -90, 100, 21, -35, -95, 1, 36, -38, -19, 1, 6, 87]
+# result = {i: numbers[i] ** 2 for i in range(len(numbers))}
+# print(result)
+
+
+# colors = {'c1': 'Red', 'c2': 'Grey', 'c3': None, 'c4': 'Green', 'c5': 'Yellow', 'c6': 'Pink', 'c7': 'Orange',
+#           'c8': None, 'c9': 'White', 'c10': 'Black', 'c11': 'Violet', 'c12': 'Gold', 'c13': None, 'c14': 'Amber',
+#           'c15': 'Azure', 'c16': 'Beige', 'c17': 'Bronze', 'c18': None, 'c19': 'Lilac', 'c20': 'Pearl', 'c21': None,
+#           'c22': 'Sand', 'c23': None}
+# result = {k: v for k, v in colors.items() if v is not None}
+# print(result)
+
+# favorite_numbers = {'timur': 17, 'ruslan': 7, 'larisa': 19, 'roman': 123, 'rebecca': 293, 'ronald': 76, 'dorothy': 62,
+#                     'harold': 36, 'matt': 314, 'kim': 451, 'rosaly': 18, 'rustam': 89, 'soltan': 111, 'amir': 654,
+#                     'dima': 390, 'amiran': 777, 'geor': 999, 'sveta': 75, 'rita': 909, 'kirill': 404, 'olga': 271,
+#                     'anna': 55, 'madlen': 876}
+# result = {k: v for k, v in favorite_numbers.items() if 9 < v < 100}
+# print(result)
+
+
+# months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August',
+#           9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+# result = {v: k for k, v in months.items()}
+# print(result)
+
+
+# s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
+# result = {int(k): v for k, v in [i.split(':') for i in s.split()]}
+# print(result)
+
+
+# numbers = [34, 10, 4, 6, 10, 23, 90, 100, 21, 35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
+# result = {k: [v for v in range(1, k + 1) if k % v == 0] for k in numbers}
+# print(result)
