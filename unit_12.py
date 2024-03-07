dbcb9dcffa13b915e4521a8803fa8404e82564eb
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-03-07 22:03:35 +0300

    add some tasks from unit 12

--- unit_12.py
+++ unit_12.py
@@ -3,8 +3,7 @@
 # random.seed(17)   # явно устанавливаем начальное значение для генератора случайных чисел
 # for _ in range(10):
 #     print(random.randint(1, 100))
-
-
+import string
 # import random
 #
 # n = int(input())
@@ -40,4 +39,21 @@
 #     lst.append(random.randint(1, 49))
 # print(*sorted(lst))
 
+
+# import random
+#
+#
+# def generate_ip():
+#     ip = ['192', '168', str(random.choice(list(range(1, 256)))), str(random.choice(list(range(1, 256))))]
+#     return '.'.join(ip)
+
+
+# from random import choice
+#
+#
+# def generate_index():
+#     letter = string.ascii_uppercase
+#     numbers = list(range(0, 100))
+#     return (choice(letter) + choice(letter) + str(choice(numbers)) + '_' + str(choice(numbers)) + choice(letter) +
+#             choice(letter))
 
