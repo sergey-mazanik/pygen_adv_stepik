f0b87bb18213850a549fc712058a08b1b5601d7d
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-03-11 09:43:45 +0300

    add some tasks for unit 12

--- unit_12.py
+++ unit_12.py
@@ -144,3 +144,32 @@ import string
 # n, m = int(input()), int(input())
 # generate_passwords(n, m)
 
+
+# from random import uniform
+#
+# n = 10 ** 6
+# k = 0
+# s0 = 16
+# for _ in range(n):
+#     x = uniform(-2, 2)
+#     y = uniform(-2, 2)
+#
+#     if x ** 3 + y ** 4 + 2 >= 0 and 3 * x + y ** 2 <= 2:
+#         k += 1
+#
+# print((k/n)*s0)
+
+
+# from random import uniform
+#
+# n = 10 ** 6
+# k = 0
+# s0 = 4
+# for _ in range(n):
+#     x = uniform(-1, 1)
+#     y = uniform(-1, 1)
+#
+#     if x ** 2 + y ** 2 <= 1:
+#         k += 1
+#
+# print((k/n)*s0)
