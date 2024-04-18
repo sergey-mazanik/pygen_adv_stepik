130cb378fdb029131d17c6302f79596f017a9037
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-04-18 14:04:06 +0300

    add some tasks for unit 13

--- unit_13.py
+++ unit_13.py
@@ -110,3 +110,27 @@
 #         if gcd(i, j) == 1:
 #             lst.append(Fraction(i, j))
 # print(*sorted(lst), sep='\n')
+
+
+# z1 = complex(input())
+# z2 = complex(input())
+# print(f'{z1} + {z2} = {z1 + z2}')
+# print(f'{z1} - {z2} = {z1 - z2}')
+# print(f'{z1} * {z2} = {z1 * z2}')
+
+
+# numbers = [3 + 4j, 3 + 1j, -7 + 3j, 4 + 8j, -8 + 10j, -3 + 2j, 3 - 2j, -9 + 9j, -1 - 1j, -1 - 10j, -20 + 15j, -21
+#            + 1j, 1j, -3 + 8j, 4 - 6j, 8 + 2j, 2 + 3j]
+# max_mod = 0
+# for number in numbers:
+#     if abs(number) > abs(max_mod):
+#         max_mod = number
+# print(max_mod)
+# print(abs(max_mod))
+
+
+# n = int(input())
+# z1 = complex(input())
+# z2 = complex(input())
+# result = z1 ** n + z2 ** n + z1.conjugate() ** n + z2.conjugate() ** (n + 1)
+# print(result)
