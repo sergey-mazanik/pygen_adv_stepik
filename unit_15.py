a32f0e34b17e457a5917730cbcc648ea820b0397
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-05-31 10:33:15 +0300

    add some tasks for unit 15

--- unit_15.py
+++ unit_15.py
@@ -43,3 +43,96 @@
 # def info_kwargs(**kwargs):
 #     for k, v in sorted(kwargs.items()):
 #         print(f'{k}: {v}')
+
+
+# numbers = [(10, 10, 10), (30, 45, 56), (81, 39), (1, 2, 3), (12,), (-2, -4, 100), (1, 2, 99), (89, 9, 34),
+#            (10, 20, 30, -2), (50, 40, 50), (34, 78, 65), (-5, 90, -1, -5), (1, 2, 3, 4, 5, 6), (-9, 8, 4),
+#            (90, 1, -45, -21)]
+#
+#
+# def comparator(item):
+#     return sum(item) / len(item)
+#
+#
+# print(min(numbers, key=comparator), max(numbers, key=comparator), sep='\n')
+
+
+# def foo(x):
+#     return (x[0] ** 2 + x[1] ** 2) ** 0.5
+#
+#
+# points = [(-1, 1), (5, 6), (12, 0), (4, 3), (0, 1), (-3, 2), (0, 0), (-1, 3), (2, 0), (3, 0), (-9, 1), (3, 6), (8, 8)]
+# points.sort(key=foo)
+# print(points)
+
+
+# def foo(x):
+#     return max(x) + min(x)
+#
+#
+# numbers = [(10, 10, 10), (30, 45, 56), (81, 80, 39), (1, 2, 3), (12, 45, 67), (-2, -4, 100), (1, 2, 99),
+#            (89, 90, 34), (10, 20, 30), (50, 40, 50), (34, 78, 65), (-5, 90, -1)]
+# numbers.sort(key=foo)
+# print(numbers)
+
+
+# def foo(x, n=input()):
+#     return x[int(n)-1]
+#
+#
+# athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30),
+#             ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
+#
+# athletes.sort(key=foo)
+# for i in athletes:
+#     print(*i)
+
+# import math
+#
+#
+# def f1(x):
+#     return x ** 2
+#
+#
+# def f2(x):
+#     return x ** 3
+#
+#
+# def f3(x):
+#     return x ** 0.5
+#
+#
+# def f4(x):
+#     return abs(x)
+#
+#
+# def f5(x):
+#     return math.sin(x)
+#
+#
+# commands = {'квадрат': f1, 'куб': f2, 'корень': f3, 'модуль': f4, 'синус': f5}
+# n = int(input())
+# command = input()
+# print(commands[command](n))
+
+
+# def comparator(x):
+#     summa = 0
+#     for i in x:
+#         summa += int(i)
+#     return summa
+#
+#
+# lst = input().split()
+# lst.sort(key=comparator)
+# print(*lst)
+
+
+# def comparator(x):
+#     return sum([int(i) for i in str(x)])
+#
+#
+# lst = sorted([int(i) for i in input().split()])
+# lst.sort(key=comparator)
+# print(lst)
+
